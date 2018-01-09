b2fbf23982db4c9decc57fb9482c56ab4a0d61cb
link_max <link_max@outlook.com>
2018-01-09 10:37:59 +0800

    fix bugs in status builder

--- lib/src/main/java/com/stockholm/common/statusbar/StatusConstant.java
+++ lib/src/main/java/com/stockholm/common/statusbar/StatusConstant.java
@@ -2,8 +2,8 @@ package com.stockholm.common.statusbar;
 
 public final class StatusConstant {
 
-    public static final String ACTION_STATUS_SHOW = "action.stockholm.show";
-    public static final String ACTION_STATUS_DISMISS = "action.stockholm.dismiss";
+    public static final String ACTION_STATUS_SHOW = "action.stockholm.status.show";
+    public static final String ACTION_STATUS_DISMISS = "action.stockholm.status.dismiss";
 
     public static final int STATUS_GROUP_NOMAL = 0;
     public static final int STATUS_GROUP_MEDIA = 1;
